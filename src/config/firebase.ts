6a84ed45b6aa1a52cc1376d055681fbc43f0b3a3
Godwinog <melomelo247419@gmail.com>
2026-07-06 22:08:54 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBA8P5NXWYLLnn7zTaaTHo2EmZMgKg0hxM",
-  authDomain: "nexus-social-88cd4.firebaseapp.com",
-  projectId: "nexus-social-88cd4",
-  storageBucket: "nexus-social-88cd4.firebasestorage.app",
+  apiKey: "AIzaSy...",
+  authDomain: "nexus-social-8fbc4.firebaseapp.com",
+  projectId: "nexus-social-8fbc4",
+  storageBucket: "nexus-social-8fbc4.appspot.com",
   messagingSenderId: "831880382027",
-  appId: "1:831880382027:web:a6375a8023dc9d972eafce"
+  appId: "1:831880382027:web:..."
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
