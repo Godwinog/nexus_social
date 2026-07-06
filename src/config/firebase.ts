a744111562a237a68d5bb88f1d0fcfd4c7ba9a4c
Godwinog <melomelo247419@gmail.com>
2026-07-06 22:21:06 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,16 +6,18 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "nexus-social-8fbc4.firebaseapp.com",
-  projectId: "nexus-social-8fbc4",
-  storageBucket: "nexus-social-8fbc4.appspot.com",
+  apiKey: "AIzaSyBA8P5NXWYLLnn7zTaaTHo2EmZMgKg0hxM",
+  authDomain: "nexus-social-88cd4.firebaseapp.com",
+  projectId: "nexus-social-88cd4",
+  storageBucket: "nexus-social-88cd4.firebasestorage.app",
   messagingSenderId: "831880382027",
-  appId: "1:831880382027:web:..."
+  appId: "1:831880382027:web:a6375a8023dc9d972eafce"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services and export them
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
